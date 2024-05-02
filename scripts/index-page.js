6c0de69dcb5ae063eef0213f53feb39ec4fac5c4
fokjul <julia.fok86@gmail.com>
2024-05-02 07:20:48 -0700

    adds function to show time ago

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -2,31 +2,6 @@ import {comments} from './copy.js';
 
 const commentContainer = document.querySelector('.comments__container');
 
-console.log(timeAgo('2021-08-09T15:29:01+0000'));
-
-
-https://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site
-// function timeAgo(input) {
-//   const date = (input instanceof Date) ? input : new Date(input);
-//   const formatter = new Intl.RelativeTimeFormat('en');
-//   const ranges = {
-//     years: 3600 * 24 * 365,
-//     months: 3600 * 24 * 30,
-//     weeks: 3600 * 24 * 7,
-//     days: 3600 * 24,
-//     hours: 3600,
-//     minutes: 60,
-//     seconds: 1
-//   };
-//   const secondsElapsed = (date.getTime() - Date.now()) / 1000;
-//   for (let key in ranges) {
-//     if (ranges[key] < Math.abs(secondsElapsed)) {
-//       const delta = secondsElapsed / ranges[key];
-//       return formatter.format(Math.round(delta), key);
-//     }
-//   }
-// }
-
 const commentBlock = [
     {
         variableName: 'comment',
@@ -111,9 +86,35 @@ const addNewComment =(callback) => {
     commentForm.addEventListener('submit', (e) => {
         e.preventDefault();
 
+        const commentDate = new Date;
+
+        //https://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site
+        function timeAgo(input) {
+          const date = (input instanceof Date) ? input : new Date(input);
+          const formatter = new Intl.RelativeTimeFormat('en');
+          const ranges = {
+            years: 3600 * 24 * 365,
+            months: 3600 * 24 * 30,
+            weeks: 3600 * 24 * 7,
+            days: 3600 * 24,
+            hours: 3600,
+            minutes: 60,
+            seconds: 1
+          };
+          const secondsElapsed = (date.getTime() - Date.now()) / 1000;
+          for (let key in ranges) {
+            if (ranges[key] < Math.abs(secondsElapsed)) {
+              const delta = secondsElapsed / ranges[key];
+              return formatter.format(Math.round(delta), key);
+            }
+          }
+        }
+
+        const commentDateTimeAgo =  timeAgo(commentDate);
+        console.log(commentDateTimeAgo)
+
         //Validating if required are not empty
         let requiredFields = document.querySelectorAll('.required');
-        console.log(requiredFields);
         let isOk = true;
         const newComment = {
             author: "", 
